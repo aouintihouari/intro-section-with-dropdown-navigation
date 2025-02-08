d82c249a12c08a5e6decf9e408cc5f4f08bd4ec2
Houari Aouinti <169095690+aouintihouari@users.noreply.github.com>
2025-02-08 13:12:36 +0100

    Corrections maded in responsiveness and header

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# intro-section-with-dropdown-navigation
+# Frontend Mentor - Intro section with dropdown navigation solution
+
+This is a solution to the [Intro section with dropdown navigation challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/intro-section-with-dropdown-navigation-ryaPetHE5). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Frontend Mentor - Intro section with dropdown navigation solution](#frontend-mentor---intro-section-with-dropdown-navigation-solution)
+  - [Table of contents](#table-of-contents)
+  - [Overview](#overview)
+    - [The challenge](#the-challenge)
+    - [Screenshot](#screenshot)
+    - [Links](#links)
+  - [My process](#my-process)
+    - [Built with](#built-with)
+  - [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the relevant dropdown menus on desktop and mobile when interacting with the navigation links
+- View the optimal layout for the content depending on their device's screen size
+- See hover states for all interactive elements on the page
+
+### Screenshot
+
+![](./design/desktop-preview.jpg)
+
+### Links
+
+- Solution URL: [GitHub Repository](https://github.com/aouintihouari/intro-section-with-dropdown-navigation)
+- Live Site URL: [Live Demo](https://aouintihouari.github.io/intro-section-with-dropdown-navigation/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+## Author
+
+- Frontend Mentor - [@aouintihouari](https://www.frontendmentor.io/profile/aouintihouari)
--- script.js
+++ script.js
@@ -1,69 +1,69 @@
-const features = document.querySelector(".features__sublist");
-const company = document.querySelector(".company__sublist");
+const open = document.querySelector(".open");
+const close = document.querySelector(".close");
 const navigation = document.querySelector(".navigation");
 const overlay = document.querySelector(".overlay");
 
-const featureItem = document.querySelector(".features");
-const companyItem = document.querySelector(".company");
-const menu = document.querySelector(".menu");
+const featuresNameMobile = document.querySelector(".features__mobile");
+const companyNameMobile = document.querySelector(".company__mobile");
 
-function toggleSubmenu(element, container) {
-  const image = container.querySelector(".arrow");
-  if (element.classList.contains("show"))
+const featuresNameDesktop = document.querySelector(
+  ".navigation__features-desktop"
+);
+const companyNameDesktop = document.querySelector(
+  ".navigation__company-desktop"
+);
+
+function toggleDropDownMobile(element) {
+  const image = element.querySelector("img");
+  const dropDownMenu = element.querySelector("ul");
+  if (image.src.endsWith("images/icon-arrow-down.svg")) {
+    image.src = "images/icon-arrow-up.svg";
+    dropDownMenu.style.height = "auto";
+    dropDownMenu.style.display = "flex";
+  } else {
     image.src = "images/icon-arrow-down.svg";
-  else image.src = "images/icon-arrow-up.svg";
-  element.classList.toggle("show");
+    dropDownMenu.style.height = "0";
+    dropDownMenu.style.display = "none";
+  }
 }
 
-featureItem.addEventListener("click", () =>
-  toggleSubmenu(features, featureItem)
-);
-
-companyItem.addEventListener("click", () =>
-  toggleSubmenu(company, companyItem)
-);
-
-menu.addEventListener("click", () => {
-  const featuresSub = document.querySelector(".features__sublist");
-  const companySub = document.querySelector(".company__sublist");
-  const features = document.querySelector(".features");
-  const company = document.querySelector(".company");
-
-  if (menu.src.endsWith("images/icon-menu.svg")) {
-    menu.classList.remove("open");
-    menu.classList.add("close");
-    navigation.style.display = "flex";
-    overlay.style.display = "block";
-    menu.src = "images/icon-close-menu.svg";
+function toggleDropDownDesktop(element) {
+  const image = element.querySelector("img");
+  const dropDownMenu = element.querySelector("ul");
+  if (image.src.endsWith("images/icon-arrow-down.svg")) {
+    image.src = "images/icon-arrow-up.svg";
+    dropDownMenu.style.display = "flex";
   } else {
-    menu.src = "images/icon-menu.svg";
-    menu.classList.remove("close");
-    menu.classList.add("open");
-    navigation.style.display = "none";
-    overlay.style.display = "none";
+    dropDownMenu.style.display = "none";
+    image.src = "images/icon-arrow-down.svg";
   }
+}
 
-  features.addEventListener("click", () => {
-    if (featuresSub.style.display === "none") {
-      features.querySelector(".arrow").src = "images/icon-arrow-up.svg";
-      featuresSub.style.display = "flex";
-      features.style.height = "200px";
-    } else {
-      features.querySelector(".arrow").src = "images/icon-arrow-down.svg";
-      featuresSub.style.display = "none";
-      features.style.height = "auto";
-    }
-  });
+open.addEventListener("click", () => {
+  navigation.style.display = "flex";
+  overlay.style.display = "block";
+  open.style.display = "none";
+  close.style.display = "block";
+});
 
-  company.addEventListener("click", () => {
-    if (companySub.style.display === "none") {
-      company.querySelector(".arrow").src = "images/icon-arrow-up.svg";
-      companySub.style.display = "flex";
-      company.style.height = "130px";
-    } else {
-      company.querySelector(".arrow").src = "images/icon-arrow-down.svg";
-      companySub.style.display = "none";
-      company.style.height = "auto";
-    }
-  });
+close.addEventListener("click", () => {
+  navigation.style.display = "none";
+  overlay.style.display = "none";
+  close.style.display = "none";
+  open.style.display = "block";
+});
+
+featuresNameMobile.addEventListener("click", () => {
+  toggleDropDownMobile(featuresNameMobile);
+});
+
+companyNameMobile.addEventListener("click", () => {
+  toggleDropDownMobile(companyNameMobile);
+});
+
+featuresNameDesktop.addEventListener("click", () => {
+  toggleDropDownDesktop(featuresNameDesktop);
+});
+companyNameDesktop.addEventListener("click", () => {
+  toggleDropDownDesktop(companyNameDesktop);
 });
